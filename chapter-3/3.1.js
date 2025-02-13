05034ad15067f4daa4de16fd5df7046e475ccf2e
Shuvodip Das Himu <shuvodipdashimu@gmail.com>
2025-02-13 21:04:31 +0600

    second commit. pretty sure?!

---
+++ chapter-3/3.1.js
@@ -0,0 +1,17 @@
+let shoppinglist = ["Milk", "Bread", "Apples"];
+console.log(shoppinglist.length);
+shoppinglist[1] = "Bananas";
+console.log(shoppinglist);
+
+// Extra stuff
+// let favoriteFruits = ["grapefruit", "orange", "lemon"];
+// favoriteFruits.push("tangerine");
+
+// let lengthOfFavoriteFruits = favoriteFruits.push("lime");
+// console.log(favoriteFruits);
+
+let arrOfShapes = ["circle", "triangle", "rectangle", "pentagon"];
+// arrOfShapes.splice(2, 0, "square", "trapezoid");
+// console.log(arrOfShapes);
+arrOfShapes.splice(2, 2, "square", "trapezoid");
+// console.log(arrOfShapes);
